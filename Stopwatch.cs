504890899482ae64a3d38956ad2048cc348a792d
szamotine <77281802+szamotine@users.noreply.github.com>
2022-09-21 14:06:07 -0600

    Add project files.

---
+++ Stopwatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Intermediate_Udemy
+{
+    class Stopwatch
+    {
+        TimeSpan duration;
+
+        public void start()
+        {
+
+        }
+        public void stop()
+        {
+
+        }
+    }
+}
